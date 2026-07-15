c4310ff7cac5324dd20442c90d22ceb4cdb3042e
Angel Rios <angel.rfm.123@gmail.com>
2026-07-14 18:06:44 -0600

    refactor: reorganizar estructura del proyecto

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,11 +1,34 @@
-function Home() {
+import Navbar from "./components/layouts/Navbar";
+
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Experience from "./sections/Experience";
+import Technologies from "./sections/Technologies";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+
+function App() {
   return (
-    <section className="flex h-[90vh] items-center justify-center">
-      <h1 className="text-6xl font-bold">
-        Bienvenido a mi Portafolio
-      </h1>
-    </section>
+    <>
+      <Navbar />
+
+      <main>
+
+        <Hero />
+
+        <About />
+
+        <Experience />
+
+        <Technologies />
+
+        <Projects />
+
+        <Contact />
+
+      </main>
+    </>
   );
 }
 
-export default Home;
+export default App;
